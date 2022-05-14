81cfca0a59455deee2670dff0a236b376d6201ce
doctormin <doctormin@sjtu.edu.cn>
2022-05-14 14:37:43 +0800

    Add feature - smem hammer

--- src/gpu_hammer.cu
+++ src/gpu_hammer.cu
@@ -1,33 +1,75 @@
 #include <cstdio>
 #include <cstdlib>
 #include "cuda_runtime.h"
-
+#include <chrono>
 #include "kernels.h"
-#include "common.h"
+#include <iostream>
+#include <iomanip>
 
+#define CUDACHECK(cmd)                                     \
+  do                                                       \
+  {                                                        \
+    cudaError_t err = cmd;                                 \
+    if (err != cudaSuccess)                                \
+    {                                                      \
+      printf("CUDA failure %s:%d '%s'\n",                  \
+             __FILE__, __LINE__, cudaGetErrorString(err)); \
+      exit(1);                                             \
+    }                                                      \
+  } while (0)
+
+#ifndef _COLORS_
+#define _COLORS_
+
+/* FOREGROUND */
+#define RST "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+#define KYEL "\x1B[33m"
+#define KBLU "\x1B[34m"
+#define KMAG "\x1B[35m"
+#define KCYN "\x1B[36m"
+#define KWHT "\x1B[37m"
+
+#define FRED(x) KRED x RST
+#define FGRN(x) KGRN x RST
+#define FYEL(x) KYEL x RST
+#define FBLU(x) KBLU x RST
+#define FMAG(x) KMAG x RST
+#define FCYN(x) KCYN x RST
+#define FWHT(x) KWHT x RST
+
+#define BOLD(x) "\x1B[1m" x RST
+#define UNDL(x) "\x1B[4m" x RST
+
+#endif /* _COLORS_ */
 
 int main(int argc, const char *argv[])
 {
-    int num_sm;
-    CUDACHECK(cudaDeviceGetAttribute(&num_sm, cudaDevAttrMultiProcessorCount, 0));
-
-    // CUDACHECK(fp32_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(l2_ld_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(gmem_ld_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(fp_hammer(cudaStreamDefault, 108));
-    CUDACHECK(gmem_fp_hammer(cudaStreamDefault, 108));
-
-    // CUDACHECK(tensor_f16f16f16_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(tensor_f16f16f32_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(tensor_bf16bf16f32_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(tensor_tf32tf32f32_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(tensor_f64f64f64_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(tensor_s8s8s32_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(tensor_s4s4s32_hammer(cudaStreamDefault, 108));
-    // CUDACHECK(tensor_b1b1s32_hammer(cudaStreamDefault, 108));
-
-    CUDACHECK(cudaDeviceSynchronize());
-
-    cudaDeviceReset();
-    return 0;
+  std::cout << "\t[" << FRED("GPU Hammer - smem hammer") << "] start" << std::endl;
+  auto startTime = std::chrono::system_clock::now().time_since_epoch().count();
+  // CUDACHECK(fp_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(gmem_fp_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(tensor_f16f16f16_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(tensor_f16f16f32_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(tensor_bf16bf16f32_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(tensor_tf32tf32f32_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(tensor_f64f64f64_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(tensor_s8s8s32_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(tensor_s4s4s32_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(tensor_b1b1s32_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(fp32_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(cudaDeviceSetCacheConfig(cudaFuncCachePreferL1));
+  // CUDACHECK(l2_ld_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(gmem_ld_hammer(cudaStreamDefault, 108));
+  // CUDACHECK(smem_ldhammer(cudaStreamDefault, 108));
+  CUDACHECK(cudaDeviceSynchronize());
+  auto endTime = std::chrono::system_clock::now().time_since_epoch().count();
+  std::cout << "\t[" << FRED("GPU Hammer") << "] "
+            << "start time = " << std::setprecision(16) << startTime / 1e9 << ", "
+            << "end time = " << endTime / 1e9 << ", "
+            << "elasped time = " << (endTime - startTime) / 1e9
+            << std::endl;
+  cudaDeviceReset();
+  return 0;
 }
---
+++ src/kernel_smem.cu
@@ -0,0 +1,38 @@
+#include "cuda_runtime.h"
+
+#define CACHED_ARRAY_SIZE  49152    // 48KB
+#define BLOCK_SIZE     128
+
+// Referred to code in https://arxiv.org/pdf/1804.06826.pdf
+static __global__ void smem_ld_hammer_kernel()
+{
+    __shared__ char arr[CACHED_ARRAY_SIZE];
+    constexpr int ntmo = BLOCK_SIZE - 1;
+    constexpr int nd = CACHED_ARRAY_SIZE / 8;
+    double x = 0;
+    int tid = threadIdx.x;
+    for (int it = 0; it < 12000000; ++it) {
+        double *ptr = (double *)arr;
+        for (int i = 0; i < nd; i += BLOCK_SIZE) {
+            #pragma unroll
+            for (int j = 0; j < BLOCK_SIZE; j += 32) {
+                int offset = (tid + j) & ntmo;
+                x += ptr[offset];
+            }
+            ptr += 32;
+        }
+    }
+    // For avoiding compiler optimization.
+    ((double *)arr)[tid] = x;
+}
+
+extern "C" {
+
+cudaError_t smem_ld_hammer(cudaStream_t s, int nblks)
+{
+    dim3 grid = dim3(nblks, 1, 1);
+    dim3 block = dim3(BLOCK_SIZE, 1, 1);
+    return cudaLaunchKernel((void *)smem_ld_hammer_kernel, grid, block, 0, 0, s);
+}
+
+} // extern "C"
