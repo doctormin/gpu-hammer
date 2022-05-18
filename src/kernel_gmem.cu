28babca186f40a14e5b12ba79d73d1f28a156692
doctormin <doctormin@sjtu.edu.cn>
2022-05-18 20:18:27 +0000

    Add gmem profile version

--- src/kernel_gmem.cu
+++ src/kernel_gmem.cu
@@ -3,7 +3,7 @@
 // Should be multiple of 32
 #define ARRAY_SIZE      268435456   // 256M
 // Should be multiple of 32 and power of 2
-#define NUM_THREADS     256
+#define BLOCK_SIZE     640
 //
 #define UNROLL_DEPTH    128
 
@@ -11,15 +11,14 @@ static __device__ char ld_arr[ARRAY_SIZE];
 
 static __global__ void gmem_ld_hammer_kernel()
 {
-    constexpr int ntmo = NUM_THREADS - 1;
     int nd = ARRAY_SIZE >> 3;
     volatile double *ptr = (volatile double *)ld_arr;
     double x = 0;
-    int idx = threadIdx.x + blockIdx.x * NUM_THREADS;
-    int sz = NUM_THREADS * gridDim.x;
+    int idx = threadIdx.x + blockIdx.x * BLOCK_SIZE;
+    int sz = BLOCK_SIZE * gridDim.x;
     int usz = sz * UNROLL_DEPTH;
     nd -= (nd % usz);
-    for (int it = 0; it < 1000; ++it) {
+    for (int it = 0; it < 100000; ++it) {
         for (int i = idx; i < nd; i += usz) {
             #pragma unroll
             for (int j = 0; j < UNROLL_DEPTH; ++j) {
@@ -36,7 +35,7 @@ extern "C" {
 cudaError_t gmem_ld_hammer(cudaStream_t s, int nblks)
 {
     dim3 grid = dim3(nblks, 1, 1);
-    dim3 block = dim3(NUM_THREADS, 1, 1);
+    dim3 block = dim3(BLOCK_SIZE, 1, 1);
     return cudaLaunchKernel((void *)gmem_ld_hammer_kernel, grid, block, 0, 0, s);
 }
 
